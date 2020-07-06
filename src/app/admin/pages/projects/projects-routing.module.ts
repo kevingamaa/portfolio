04484f0521c50a805f76ab8a30257a972a11cce2
Vitor kevin <vitorkortez@gmail.com>
2020-07-06 17:30:33 -0300

    update - dashboard admin

--- src/app/admin/pages/projects/projects-routing.module.ts
+++ src/app/admin/pages/projects/projects-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectComponent } from 'src/app/website/projects/project/project.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: ProjectComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
